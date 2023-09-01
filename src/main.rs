bd90a6caf9f9473ee6199adaf25b0844aa92039f
HuangWei <market@zuiyue.com>
2023-09-01 16:11:13 +0800

    0.1.2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-use std::env::args;
-use url::Url;
-
-use tokio::fs;
-use tokio::io::AsyncWriteExt;
-
-use serde_json::json;
-use reqwest::header::{HeaderMap, HeaderValue, SET_COOKIE, COOKIE};
+// use serde_json::json;
+use std::path::Path;
+use std::process::Command;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -13,29 +8,44 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args: Vec<String> = std::env::args().collect();
     
-    if args.len() < 2 {
-        help();
-        return Ok(())
+
+    let mut command = "";
+
+    if args.len() > 1 {
+        command = args[1].as_str();
     }
 
-    let command = args[1].as_str();
-
+    let main_url = "http://127.0.0.1:10001/";
     let client = reqwest::Client::builder().build()?;
-    let main_url = "http://frp004.xlai.cc:8080/";
-
-    let login_url = main_url.to_owned() + "api/v2/auth/login";
-    let form = [("username", "admin"), ("password", "adminadmin")];
-    let data = client.post(login_url).form(&form).send().await?;
-    
-    let cookies = data.headers().get_all(SET_COOKIE).iter()
-        .map(|value| value.to_str().unwrap().to_owned())
-        .collect::<Vec<String>>().join("; ");
-    let mut headers = HeaderMap::new();
-    headers.insert(COOKIE, HeaderValue::from_str(&cookies)?);
 
     match command {
+        "run" => {
+            run().await?;
+        }
         "add" => {
-            add().await?;
+            if args.len() < 4 {
+                help();
+                return Ok(());
+            }
+        
+            let url = args[2].clone();
+            let path = args[3].clone();
+        
+            let form = reqwest::multipart::Form::new()
+                .text("urls", url)
+                .text("autoTMM", "false")
+                .text("savepath", path)
+                .text("rename", "")
+                .text("category", "")
+                .text("paused", "false")
+                .text("stopCondition", "None")
+                .text("contentLayout", "Original")
+                .text("dlLimit", "NaN")
+                .text("upLimit", "NaN");
+        
+            let add_torrent_url = main_url.to_owned() + "api/v2/torrents/add";
+            let data = client.post(add_torrent_url).multipart(form).send().await?.text().await?;
+            println!("add: {:?}", data);
         },
         "get" => {
             if args.len() < 3 {
@@ -44,7 +54,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
             let hash = args[2].as_str();
             let url = main_url.to_owned() + "api/v2/torrents/info?hashes=" + hash;
-            let data = client.get(url).headers(headers).send().await?.text().await?;
+            let data = client.get(url).send().await?.text().await?;
             println!("get: {:?}", data);
         },
         "del" => {
@@ -57,17 +67,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let form = reqwest::multipart::Form::new()
                 .text("hashes", hash)
                 .text("deleteFiles", "true");
-            let data = client.post(url).headers(headers).multipart(form).send().await?.text().await?;
+            let data = client.post(url).multipart(form).send().await?.text().await?;
             println!("del: {:?}", data);
         },
         "list" => {
             let url = main_url.to_owned() + "api/v2/torrents/info";
-            let data = client.get(url).headers(headers).send().await?.text().await?;
+            let data = client.get(url).send().await?.text().await?;
             println!("list: {:?}", data);
         },
-        _ => {
+        "quit" => {
+            let main_url = "http://127.0.0.1:10001/";
+            let client = reqwest::Client::builder().build()?;
+            let url = main_url.to_owned() + "api/v2/app/shutdown";
+            let data = client.post(url).send().await?.text().await?;
+            println!("quit: {:?}", data);
+        }
+        "help" => {
             help();
-            return Ok(());
+        }
+        _ => {
+            run().await?;
         }
     }
 
@@ -77,79 +96,90 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn help() {
     let args: Vec<String> = std::env::args().collect();
     println!("Usage:");
-    println!("  {} add <url>", args[0]);
+    println!("  {} run", args[0]);
+    println!("  {} add <url> <savepath>", args[0]);
     println!("  {} get <hash>", args[0]);
     println!("  {} del <hash>", args[0]);
     println!("  {} list", args[0]);
-}
-
-async fn add() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 3 {
-        help();
-        return Ok(());
-    }
-
-    let client = reqwest::Client::builder().build()?;
-    let main_url = "http://frp004.xlai.cc:8080/";
-
-    let login_url = main_url.to_owned() + "api/v2/auth/login";
-    let form = [("username", "admin"), ("password", "adminadmin")];
-    let data = client.post(login_url).form(&form).send().await?;
-    
-    let cookies = data.headers().get_all(SET_COOKIE).iter()
-        .map(|value| value.to_str().unwrap().to_owned())
-        .collect::<Vec<String>>().join("; ");
-    let mut headers = HeaderMap::new();
-    headers.insert(COOKIE, HeaderValue::from_str(&cookies)?);
-
-    let url = args[2].as_str();
-
-    // 获取url的文件名
-    let url_filename = Url::parse(url).unwrap();
-    let segments = url_filename.path_segments().map(|c| c.collect::<Vec<_>>()).unwrap();
-    let file_name = segments.last().unwrap();
-
-    // 下载文件并保存文件
-    let response = reqwest::get(url).await?;
-    fs::create_dir_all("./download/").await?;
-    let path = format!("./download/{}", file_name.clone());
-    let mut out = fs::File::create(path).await?;
-    let bytes = response.bytes().await?;
-    out.write_all(&bytes).await?;
-
-    // 上传文件到API接口
-    let path = format!("./download/{}", file_name.clone());
-    let file = std::path::Path::new(&path);
-    let bytes = std::fs::read(file).expect("Unable to read file");
-    
-    let part = reqwest::multipart::Part::bytes(bytes)
-        .file_name(file_name.clone().to_owned())
-        .mime_str("application/x-bittorrent")?;
-
-    let form = reqwest::multipart::Form::new()
-        .part("fileselect[]", part)
-        .text("autoTMM", "false")
-        .text("savepath", "/home/")
-        .text("rename", "")
-        .text("category", "")
-        .text("paused", "false")
-        .text("stopCondition", "None")
-        .text("contentLayout", "Original")
-        .text("dlLimit", "NaN")
-        .text("upLimit", "NaN");
-
-    let add_torrent_url = main_url.to_owned() + "api/v2/torrents/add";
-    let data = client.post(add_torrent_url).headers(headers).multipart(form).send().await?.text().await?;
-    println!("add: {:?}", data);
-    Ok(())
+    println!("  {} help", args[0]);
 }
 
 
 // 查询当前系统，win or linux or mac
 // 执行./qbittorrent/qbittorrent<取决于系统增加.exe后缀> 失败则运行
 // 执行../wei-release/windows/qbittorrent/qbittorrent.exe 失败则下载
-// get https://download.zuiyue.com/windows/qbittorrent/qbittorrent.exe
+// get http://download.zuiyue.com/windows/qbittorrent/qbittorrent.exe
+async fn run() -> Result<(), Box<dyn std::error::Error>> {
+    use single_instance::SingleInstance;
+    let instance = SingleInstance::new("wei-qbittorrent").unwrap();
+    if !instance.is_single() { 
+        std::process::exit(1);
+    };
+
+    let main_url = "http://127.0.0.1:10001/";
+    let client = reqwest::Client::builder().build()?;
+    let url = main_url.to_owned() + "api/v2/app/shutdown";
+    client.post(url).send().await?.text().await?;
+
+    let os = std::env::consts::OS;
+    let command = match os {
+        "windows" => "./qbittorrent/qbittorrent.exe",
+        _ => "./qbittorrent/qbittorrent",
+    };
+
+    // 复制./qbittorrent/qbittorrent.ini 到 -> AppData/Roaming/qBittorrent/qBittorrent.ini
+    let home = std::env::var("USERPROFILE").unwrap();
+    let qbittorrent_ini = format!("{}/AppData/Roaming/qBittorrent/qBittorrent.ini", home);
+    let qbittorrent_ini_path = Path::new(&qbittorrent_ini);
+    if !qbittorrent_ini_path.exists() {
+        let qbittorrent_ini_dir = format!("{}/AppData/Roaming/qBittorrent", home);
+        let qbittorrent_ini_dir_path = Path::new(&qbittorrent_ini_dir);
+        if !qbittorrent_ini_dir_path.exists() {
+            std::fs::create_dir_all(qbittorrent_ini_dir_path).unwrap();
+        }
+        let source = "./qbittorrent/qBittorrent.ini";
+        let source_path = Path::new(source);
+        if source_path.exists() {
+            std::fs::copy(source_path, qbittorrent_ini_path).unwrap();
+        }
+    }
+
+    match Command::new(command).output() {
+        Ok(output) => {
+            println!("output: {:?}", output);
+        }
+        Err(_) => {
+            download("http://download.zuiyue.com/windows/qbittorrent/qbittorrent.exe").await;
+            download("http://download.zuiyue.com/windows/qbittorrent/qBittorrent.ini").await;
+            download("http://download.zuiyue.com/windows/qbittorrent/Qt6Core.dll").await;
+            download("http://download.zuiyue.com/windows/qbittorrent/Qt6Network.dll").await;
+            download("http://download.zuiyue.com/windows/qbittorrent/Qt6Sql.dll").await;
+            download("http://download.zuiyue.com/windows/qbittorrent/Qt6Xml.dll").await;
+        }
+    }
+
+    Ok(())
+} 
 
 // 检查windows系统是否安装了qbittorrent,并且是否开启了端口10001,如果没有
 // 检查系统进程是否存在qbittorrent,如果不存在则运行qbittorrent.检查api端口是否开启成功
+
+
+// 下载文件，并存放到指定目录
+async fn download(url_str: &str) {
+    let url = reqwest::Url::parse(url_str).unwrap();
+
+    let dir = "./qbittorrent";
+    let file_path = format!("{}/{}", dir, url.path_segments().unwrap().last().unwrap());
+
+    // Create directory if it doesn't exist
+    if !Path::new(dir).exists() {
+        std::fs::create_dir_all(dir).unwrap();
+    }
+
+    let response = reqwest::get(url).await.unwrap();
+
+    let content = response.bytes().await.unwrap();
+    std::fs::write(&file_path, content).expect("Unable to write file");
+    println!("Download completed!");    
+}
